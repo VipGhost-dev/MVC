bbbf09b1b6e038247ab8ef2111b00fb2d6a88be4
Егор <lied00@mail.ru>
2023-01-16 14:13:56 +0300

    Добавьте файлы проекта.

---
+++ MVC/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    internal class Model
+    {
+        public double FirstNumber;
+        public double SecondNumber;
+        public static List<string> Symbols = new List<string> {"Сложение", "Вычитание", "Умножение", "Деление"};
+
+        public static TextBlock tbSymbol;
+        public static TextBox tbxResult;
+    }
+}
